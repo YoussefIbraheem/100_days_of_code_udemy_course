088d2e556e1618ce5d47a1fdf9aac1f8670322f0
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-05-11 16:43:08 +0300

    add: initialize project configuration files and set up Python SDK

---
+++ turtle-crossing-start/player.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.color('black')
+        self.penup()
+        self.teleport(*STARTING_POSITION)
+        self.setheading(90)
+
+
+    def move_forward(self):
+        if self.heading() != 90:
+            self.setheading(90)
+        self.goto(self.xcor() , self.ycor() + MOVE_DISTANCE)
+
+    def move_backwards(self):
+        if self.heading() != 270:
+            self.setheading(270)
+        self.goto(self.xcor() , self.ycor() - MOVE_DISTANCE)
