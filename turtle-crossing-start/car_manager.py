5f038cf045249edde936f769d00e34cb9f8a47e5
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-05-12 11:54:43 +0300

    add: implement car generation and management in the game

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,7 +1,31 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
-
+from turtle import Turtle
+import random
 
 class CarManager:
-    pass
+
+    __COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+    __STARTING_MOVE_DISTANCE = 5
+
+    __MOVE_INCREMENT = 10
+
+    def __init__(self,cars_amount):
+        self.generate_cars(cars_amount=cars_amount)
+
+
+    def __generate_car(self, position):
+        car = Turtle()
+        car.shape('square')
+        car.shapesize(stretch_wid=1,stretch_len=2)
+        car.color(random.choice(self.__COLORS))
+        car.teleport(position)
+
+    def generate_cars(self, cars_amount:int):
+        for _ in range(cars_amount):
+            position = (280 , random.randint(-280,280))
+            self.__generate_car(position)
+
+
+
+
+
--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -7,10 +7,14 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+
 player = Player()
 screen.listen()
 screen.onkey(player.move_forward , 'w')
 screen.onkey(player.move_backwards,'s')
+car =CarManager(10)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
