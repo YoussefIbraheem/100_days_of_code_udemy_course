80c8db9138e92edb5f4a2fbf67083c3bcd46754b
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-06-27 01:23:42 +0300

    add: implement the highest score tracking and display in scoreboard

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -5,8 +5,15 @@ class Scoreboard(Turtle):
     __ALIGNMENT = "center"
 
     def __init__(self):
+        """Initializes the Scoreboard with default values and sets up the display."""
         super().__init__()
         self.__score = 0
+        with open("data.txt", "r") as file:
+            try:
+                self.__highest_score = int(file.read())
+                file.close()
+            except ValueError:
+                self.__highest_score = 0
         self.hideturtle()
         self.color('white')
         self.penup()
@@ -19,15 +26,30 @@ class Scoreboard(Turtle):
     def set_score(self,scr:int):
         self.__score = scr
 
+    def get_highest_score(self):
+        return self.__highest_score
+
+    def set_highest_score(self,highest_score):
+        self.__highest_score = highest_score
+        with open("data.txt", "w") as file:
+            file.write(str(self.__highest_score))
+            file.close()
+
+
+
     def increment_score(self):
         self.__score += 1
         self.refresh_board()
 
     def refresh_board(self):
         self.clear()
-        self.write(f"Score:{self.__score}", False, self.__ALIGNMENT, (self.__FONT, 18, "bold"))
+        self.write(f"Score:{self.__score}", False, 'left', (self.__FONT, 18, "bold"))
+        self.write(" " * 20, False, 'center', (self.__FONT, 18, "bold"))
+        self.write(f"Highest Score:{self.__highest_score}", False, 'right', (self.__FONT, 18, "bold"))
 
     def game_over(self):
         self.clear()
         self.home()
-        self.write("Game Over", False, self.__ALIGNMENT, (self.__FONT, 24, "bold"))
+        if self.get_score() > self.get_highest_score():
+            self.set_highest_score(self.get_score())
+        self.write("Game Over \n Highest Score: " + str(self.get_highest_score()), False, self.__ALIGNMENT, (self.__FONT, 18, "bold"))
