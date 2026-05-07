6711bceaf1da17f5e07be80f2e5ae28da5ed013a
YoussefIbraheem <youssefm.muhammed@gmail.com>
2026-05-08 01:35:21 +0300

    Add search functionality to find saved website credentials

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,7 +1,34 @@
+import json
 import random
 import tkinter as tk
 from tkinter import messagebox
-import json
+
+
+# ---------------------------- SEARCH ------------------------------- #
+def search():
+    keyword = website_input.get()
+
+    try:
+        with open("./data.json", mode="r", encoding="utf-8") as f:
+            data: dict = json.load(f)
+
+    except FileNotFoundError:
+        messagebox.showerror("Not Found", "File does not exist!")
+
+    else:
+        located_data = data.get(keyword, None)
+
+        if not located_data:
+            messagebox.showwarning("Not Found", "No website data found!")
+            return
+
+        messagebox.showinfo(
+            f"{keyword}",
+            f"Email/Username: {located_data['email_username']}\nPassword:{located_data['password']}",
+        )
+
+    finally:
+        website_input.delete(0, tk.END)
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -38,23 +65,22 @@ def save_password():
         messagebox.showwarning("Missing Data", "Password is required")
         return
 
-    new_data = {website: {"email_username": eu, "password": password} }
+    new_data = {website: {"email_username": eu, "password": password}}
 
     try:
-
-        with open("./data.json",mode="r", encoding="utf-8") as f:
+        with open("./data.json", mode="r", encoding="utf-8") as f:
             data = json.load(f)
 
     except FileNotFoundError:
-        with open("./data.json",mode="w", encoding="utf-8") as f:
-            json.dump(new_data,f,indent=4)
+        with open("./data.json", mode="w", encoding="utf-8") as f:
+            json.dump(new_data, f, indent=4)
             messagebox.showinfo("Success", "Password saved successfully")
     except Exception:
         raise ValueError("An error occurred while saving the password.")
     else:
         data.update(new_data)
-        with open("./data.json",mode="w", encoding="utf-8") as f:
-            json.dump(data,f,indent=4)
+        with open("./data.json", mode="w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
             messagebox.showinfo("Success", "Password saved successfully")
     finally:
         website_input.delete(0, tk.END)
@@ -72,7 +98,7 @@ root.resizable(width=False, height=False)
 logo_img = tk.PhotoImage(file="logo.png")
 canvas = tk.Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_img)
-website_input = tk.Entry(width=35)
+website_input = tk.Entry(width=21)
 website_input.focus()
 website_label = tk.Label(text="Website")
 eu_input = tk.Entry(width=35)
@@ -87,14 +113,18 @@ gen_pass_btn = tk.Button(
     command=generate_password,
 )
 add_btn = tk.Button(text="Add", width=35, padx=0, command=save_password)
+search_btn = tk.Button(
+    text="Search", width=14, font=("Arial", 8, "normal"), command=search
+)
 
 website_label.grid(row=1, column=0)
 eu_label.grid(row=2, column=0)
 password_label.grid(row=3, column=0)
 canvas.grid(row=0, column=1, columnspan=2)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1, sticky="w")
 eu_input.grid(row=2, column=1, columnspan=2)
 password_input.grid(row=3, column=1, sticky="w")
 gen_pass_btn.grid(row=3, column=2)
 add_btn.grid(row=4, column=1, columnspan=2)
+search_btn.grid(row=1, column=2)
 root.mainloop()
